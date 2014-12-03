fd76f64f3352cd0233d909df1cb0b654d6106c4a
oggs91 <dschoerk@gmx.at>
2014-12-03 18:26:06 +0100

    la musica

---
+++ Assets/InstrumentScript.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class InstrumentScript : MonoBehaviour {
+
+	public bool active = false;
+	public AudioSource source;
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (source == null)
+			return;
+
+		if (active && !source.isPlaying) 
+			source.Play();
+		else if(!active && source.isPlaying)
+			source.Pause();
+	}
+}
